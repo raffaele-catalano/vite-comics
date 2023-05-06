3b0f0724cb8d58f23d630870086bbe51084b2d10
raffaele-catalano <raf.catalano@gmail.com>
2023-05-06 12:43:51 +0200

    setup Header markup + some changes in scss

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,9 +5,63 @@ export default {
 </script>
 
 <template>
-    
+    <header>
+        <div class="header-wrapper">
+            <div class="logo-container">
+                <img src="../assets/img/dc-logo.png" alt="">
+            </div>
+            <div class="nav-container">
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="#">Prova</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
 </template>
 
 <style lang="scss" scoped>
+@use '../scss/main.scss' as *;
+// @use '../scss/partials/variables' as *;
 
+header {
+    display: flex;
+    justify-content: center;
+    height: 120px;
+    width: 100%;
+    background-color: $light;
+    margin-top: 20px;
+    .header-wrapper {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 65%;
+
+        .logo-container {
+            img {
+                width: 80%;
+            }
+        }
+        .nav-container {
+            
+            nav {
+                ul {
+                    li {
+                        display: inline-block;
+                        a {
+                            color: $gray;
+                            margin: 0 10px;
+                            text-transform: uppercase;
+                            font-size: $s-txt;
+                            font-weight: $bold;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
 </style>
